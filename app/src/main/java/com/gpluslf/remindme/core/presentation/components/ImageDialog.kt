33d82c31bdae6f2e28e28b7f8d5f3726d324fd63
SadAlexa <alessiacarfi@proton.me>
2025-02-01 19:23:07 +0100

    changed ImageDialog from AlertDialog to Dialog

--- app/src/main/java/com/gpluslf/remindme/core/presentation/components/ImageDialog.kt
+++ app/src/main/java/com/gpluslf/remindme/core/presentation/components/ImageDialog.kt
@@ -2,30 +2,28 @@ package com.gpluslf.remindme.core.presentation.components
 
 import android.net.Uri
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.AlertDialog
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import coil.compose.rememberAsyncImagePainter
 
 @Composable
 fun ImageDialog(imageUri: Uri, onDismiss: () -> Unit) {
-    AlertDialog(
+    Dialog(
         onDismissRequest = onDismiss,
-        text = {
-            Image(
-                painter = rememberAsyncImagePainter(imageUri),
-                contentDescription = null,
-                modifier = Modifier
-                    .size(350.dp)
-                    .clip(RoundedCornerShape(30.dp))
-            )
-        },
-        confirmButton = {},
-        containerColor = Color.Transparent
-    )
+        properties = DialogProperties(usePlatformDefaultWidth = false)
+    ) {
+        Image(
+            painter = rememberAsyncImagePainter(imageUri),
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxWidth(0.8f)
+                .clip(RoundedCornerShape(30.dp))
+        )
+    }
 }
